b32bb56d9f8b14d75bcdf809e032fc687d113f88
joy_03 <85474393+whdldi@users.noreply.github.com>
2022-02-24 15:52:46 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -104,21 +104,21 @@ $(function () {
     if ($("input[name='chkbox']").is(":checked") == true) {
       setCookie("close", "Y", 1); //기간( ex. 1은 하루, 7은 일주일)
     }
-    $("#pop").hide();
+    $("#pop").fadeOut(0);
   }
 
   $(document).ready(function () {
     var cookiedata = document.cookie;
     console.log(cookiedata);
     if (cookiedata.indexOf("close=Y") < 0) {
-      $("#pop").fadeIn();
+      $("#pop").show();
     } else {
-      $("#pop").hide();
+      $("#pop").fadeOut(200);
     }
     $(".btnClose").click(function () {
       couponClose();
       $("#sli").hide();
-      $(".pop_slick").fadeOut();
+      $(".pop").fadeOut(200);
       //모든 팝업에서 X눌렀을때 닫기
     });
     $("#close_ok").click(function () {
@@ -126,48 +126,44 @@ $(function () {
     });
     $("#close_letsee").click(function () {
       couponClose();
-      $("#sli").fadeIn(300);
-      //볼래요 눌렀을때 슬릭 팝업 천천히 표출
+      $("#popserv").show();
+      //볼래요 눌렀을때 서비스팝업3가지중 첫번째팝업
     });
+
+
+    //이하 다음버튼/이전버튼 이동
+    $("#next_1").click(function () {
+      couponClose();
+      $("#popserv").hide();
+      $('#popserv2').show();
+    });
+
+    $("#prev_2").click(function () {
+      couponClose();
+      $("#popserv2").hide();
+      $('#popserv').show();
+    });
+
+    $("#next_2").click(function () {
+      couponClose();
+      $("#popserv2").hide();
+      $('#popserv3').show();
+    });
+
+    $("#prev_3").click(function () {
+      couponClose();
+      $("#popserv3").hide();
+      $('#popserv2').show();
+    });
+
+    $("#next_3").click(function () {
+      couponClose();
+      $("#popserv3").hide();
+      $('#popserv4').show();
+    });
+
+
   });
 });
 
 
- /* popup 슬라이드 */
- $(function(){
-  $('.pop_sli_wrap').slick({
-       dots: true,
-       infinite: false,
-       speed: 800,
-       slidesToShow: 1,
-       slidesToScroll: 1,
-       autoplay:false,
-       responsive: [
-         {
-           breakpoint: 1024,
-           settings: {
-             slidesToShow: 1,
-             slidesToScroll: 1,
-             infinite: true,
-             dots: true
-           }
-         },
-         {
-           breakpoint: 600,
-           settings: {
-             slidesToShow: 1,
-             slidesToScroll: 1
-           }
-         },
-         {
-           breakpoint: 360,
-           settings: {
-             slidesToShow: 1,
-             slidesToScroll: 1
-           }
-         }
-       ]
-     });
-});
-
-      
